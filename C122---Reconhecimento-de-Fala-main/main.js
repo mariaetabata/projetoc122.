cb66143fcbbb5bdc8f1a54b4ddc0289c731c3e54
mariaetabata <139805462+mariaetabata@users.noreply.github.com>
2023-07-20 15:28:47 -0300

    Add files via upload

---
+++ C122---Reconhecimento-de-Fala-main/main.js
@@ -0,0 +1,12 @@
+var screenWidth = 0;
+var screenHeight = 0;
+
+var apple = "";
+
+var speechData = "";
+var toNumber = "";
+
+function preload = "loadImage()";
+
+to_number = Number(content);
+if(Number.isInteger(to_number));
